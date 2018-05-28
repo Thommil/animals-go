de552a4622697d91b931d6f76c15604c4c717467
Thomas Millet <thommil@pc67.home>
2018-05-28 16:43:35 +0200

    Fixed common error

--- env.sh
+++ env.sh
@@ -4,6 +4,5 @@ export GOPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PATH=$PATH:$GOPATH/bin
 
 #Dependencies
-go get -d github.com/thommil/animals-go-common
 go get -d github.com/thommil/animals-go-auth
 go get -d github.com/thommil/animals-go-ws
